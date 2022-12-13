155cccf90e2f2b9084b55e94f749bb15093358d3
Denis Ponizovskiy <denfromdark@yandex.ru>
2022-12-13 21:24:21 +0300

    try to run frontend

---
+++ devops/deploy_front.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Если свалится одна из команд, рухнет и весь скрипт
+set -xe
+#Перезаливаем дескриптор сервиса на ВМ для деплоя
+sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
+#sudo rm -f /home/jarservice/sausage-store.jar||true
+#Переносим артефакт в нужную папку
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.tar.gz ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+sudo tar -C /var/www-data/${VERSION} -xvf sausage-store-${VERSION}.tar.gz | sudo cp /var/www-data/${VERSION}/frontend/* /var/www-data/
+#sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
+sudo systemctl daemon-reload
+#Перезапускаем сервис сосисочной
+sudo systemctl restart sausage-store-frontend
