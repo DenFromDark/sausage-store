4bce9467c42c9352cb32bd143fae401d508c8edc
Denis Ponizovskiy <denfromdark@yandex.ru>
2022-12-13 23:14:52 +0300

    try to run frontend

--- devops/deploy_front.sh
+++ devops/deploy_front.sh
@@ -5,7 +5,8 @@ set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 #sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.tar.gz ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.tar.gz \
+    ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo tar -C /var/www-data/${VERSION} -xvf sausage-store-${VERSION}.tar.gz | sudo cp /var/www-data/${VERSION}/frontend/* /var/www-data/
 #sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
