b93cdc3f3ba1c4005f53ee288286dd0d1b5e0498
Denis Ponizovskiy <denfromdark@yandex.ru>
2022-12-14 01:19:23 +0300

    try to run frontend

--- devops/deploy_back.sh
+++ devops/deploy_back.sh
@@ -6,7 +6,8 @@ sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-back
 rm -f /home/jarservice/sausage-store.jar||true && \
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && \
-cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true && \ #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
+cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true && \ 
+#"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
 systemctl daemon-reload && \
 #Перезапускаем сервис сосисочной
 systemctl restart sausage-store-backend
