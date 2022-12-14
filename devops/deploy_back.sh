73c104182636bb3d8a3a46e4741e013d523d5203
Denis Ponizovskiy <denfromdark@yandex.ru>
2022-12-14 10:49:40 +0300

    try to run frontend

--- devops/deploy_back.sh
+++ devops/deploy_back.sh
@@ -3,11 +3,11 @@
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service && \
-rm -f /home/jarservice/sausage-store.jar||true && \
+sudo rm -f /home/jarservice/sausage-store.jar||true && \
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && \
-cp /home/${DEV_USER}/sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar && \
+sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && \
+sudo cp /home/${DEV_USER}/sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar && \
 #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
-systemctl daemon-reload && \
+sudo systemctl daemon-reload && \
 #Перезапускаем сервис сосисочной
-systemctl restart sausage-store-backend
+sudo systemctl restart sausage-store-backend
