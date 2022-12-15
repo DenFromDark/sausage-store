e5f9b88c6f6ea93d04e4c80f35ad9c204ebe49a3
Denis Ponizovskiy <denfromdark@yandex.ru>
2022-12-15 19:51:53 +0300

    try to run

--- devops/deploy_back.sh
+++ devops/deploy_back.sh
@@ -5,10 +5,10 @@ set -xe
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service && \
 sudo rm -f /home/jarservice/sausage-store.jar||true && \
 #Переносим артефакт в нужную папку
-sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && \
-sudo cp /home/${DEV_USER}/sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar && \
+sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && \
+sudo cp ./sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar||true && \
 #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
-sudo chown jarservice:jarservice /home/jarservice/sausage-store.jar && \
+#sudo chown jarservice:jarservice /home/jarservice/sausage-store.jar && \
 
 sudo systemctl daemon-reload && \
 
