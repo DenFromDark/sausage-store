71a42ffdb36deebe20e8cf6bc172ea9c8c41c133
Denis Ponizovskiy <denfromdark@yandex.ru>
2022-12-15 08:58:47 +0300

    try to run

--- devops/deploy_front.sh
+++ devops/deploy_front.sh
@@ -7,7 +7,7 @@ sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-fro
 #Переносим артефакт в нужную папку
 sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o /home/${DEV_USER}/sausage-store-${VERSION}.tar.gz ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz && \
 sudo mkdir /var/www-data/${VERSION} && \
-sudo tar -C /var/www-data/${VERSION} -xvf sausage-store-${VERSION}.tar.gz && \
+sudo tar -C /var/www-data/${VERSION} -xvf /home/${DEV_USER}/sausage-store-${VERSION}.tar.gz && \
 sudo cp /var/www-data/${VERSION}/frontend/* /var/www-data/ && \
 #rm -f /var/www-data/${VERSION}
 #sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
