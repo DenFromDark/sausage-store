78cb8e3b0254bcd992f028c3421978f2ccb26f07
Denis Ponizovskiy <denfromdark@yandex.ru>
2023-05-19 14:46:09 +0300

    service secrets

--- devops/deploy_back.sh
+++ devops/deploy_back.sh
@@ -3,6 +3,9 @@
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service && \
+sudo cp /home/${DEV_USER}/sausage-store-backend.env /home/jarservice/sausage-store-backend.env && \
+sudo chown jarservice:jarervice /home/jarservice/sausage-store-backend.env && \
+sudo chmod 400 /home/jarservice/sausage-store-backend.env && \
 sudo rm -f /home/jarservice/sausage-store.jar||true && \
 #Переносим артефакт в нужную папку
 sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}sausage-store-ponizovskiy-denis-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && \
