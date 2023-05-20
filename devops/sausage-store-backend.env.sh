10a9ed0c44cc4be29dcb2783ef883850e6cc56b1
Denis Ponizovskiy <denfromdark@yandex.ru>
2023-05-20 10:16:11 +0300

    env

---
+++ devops/sausage-store-backend.env.sh
@@ -0,0 +1,2 @@
+# !/bin/bash
+echo -e ""PSQL_HOST="$PSQL_HOST\n"PSQL_PASS="$PSQL_PASS\n"PSQL_PORT="$PSQL_PORT\n"PSQL_BASE="$PSQL_BASE\n"PSQL_USER="$PSQL_USER" > /home/${DEV_USER}/sausage-store-backend.env
