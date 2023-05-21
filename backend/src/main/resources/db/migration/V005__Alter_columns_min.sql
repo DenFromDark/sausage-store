56615c511b59961af521cadfad2966f137573f6a
Denis Ponizovskiy <denfromdark@yandex.ru>
2023-05-21 12:34:16 +0300

    Flyway

--- backend/src/main/resources/db/migration/V005__Alter_columns_min.sql
+++ backend/src/main/resources/db/migration/V005__Alter_columns_min.sql
@@ -1,3 +1,2 @@
-ALTER table product	
-MODIFY COLUMN name TYPE varchar(20),
-MODIFY COLUMN picture_url TYPE varchar(100);
+ALTER table product	MODIFY COLUMN name TYPE varchar(20);
+ALTER TABLE product MODIFY COLUMN picture_url TYPE varchar(100);
