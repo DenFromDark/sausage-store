b3ecc12aec5b4601959a1be2195a18a06416d8e7
Denis Ponizovskiy <denfromdark@yandex.ru>
2023-05-21 15:36:14 +0300

    Flyway

--- backend/src/main/resources/db/migration/R__index.sql
+++ backend/src/main/resources/db/migration/R__index.sql
@@ -1,6 +1,1 @@
-DROP INDEX product_id_idx;
-DROP INDEX prod_price_idx;
-DROP INDEX prod_name_idx;
-CREATE INDEX ON product(id);
-CREATE INDEX ON product(name);
-CREATE INDEX ON product(price);
+REINDEX INDEX product_id_idx;
