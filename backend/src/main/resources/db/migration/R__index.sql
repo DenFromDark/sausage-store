7abfd68f69f9612efec8a6698c99ab083e12c939
Denis Ponizovskiy <denfromdark@yandex.ru>
2023-05-22 11:40:15 +0300

    finalize Flyway

--- backend/src/main/resources/db/migration/R__index.sql
+++ backend/src/main/resources/db/migration/R__index.sql
@@ -1,6 +1,6 @@
-DROP INDEX IF EXISTS product_id_idx ON product;
+DROP INDEX IF EXISTS product_id_idx;
 CREATE INDEX product_id_idx ON product(id);
-DROP INDEX IF EXISTS product_name_idx ON product;
+DROP INDEX IF EXISTS product_name_idx;
 CREATE INDEX product_name_idx ON product(name);
-DROP INDEX IF EXISTS product_price_idx ON product;
+DROP INDEX IF EXISTS product_price_idx;
 CREATE INDEX product_price_idx ON product(price);
