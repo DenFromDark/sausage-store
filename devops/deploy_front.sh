029449be40a596c25685f37c85c930f3d7e1a5ac
Denis Ponizovskiy <denfromdark@yandex.ru>
2023-05-24 12:32:53 +0300

    deploy frontend

--- devops/deploy_front.sh
+++ devops/deploy_front.sh
@@ -19,10 +19,9 @@
 #!/bin/bash
 set +e
 
-docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/d.ponizovskiy/sausage-store/sausage-frontend:latest
-docker stop backend || true
-docker rm backend || true
+docker stop frontend || true
+docker rm frontend || true
 set -e
 docker run -d --name frontend \
     --network=sausage_network \
