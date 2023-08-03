aa4626d1618f9f07a6545198a1627a62da7e8e27
Denis Ponizovskii <denfromdark@yandex.ru>
2023-08-03 14:40:35 +0300

    try to start

--- devops/deploy_front.sh
+++ devops/deploy_front.sh
@@ -19,7 +19,6 @@
 #!/bin/bash
 set +e
 
-docker login
 docker pull gitlab.praktikum-services.ru:5050/d.ponizovskiy/sausage-store/sausage-frontend:latest
 docker stop frontend || true
 docker rm frontend || true
