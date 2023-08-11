183c4d67a19248bd96f2338c7fe5310e3e76f16a
Denis Ponizovskii <denfromdark@yandex.ru>
2023-08-11 10:16:41 +0300

    del

--- backend/target/classes/db/migration/V004__Alter_columns_max.sql
+++
@@ -1,2 +0,0 @@
-ALTER table product	ALTER COLUMN name TYPE varchar(255);
-ALTER TABLE PRODUCT ALTER COLUMN picture_url TYPE varchar(200);
--- backend/target/classes/db/migration/V005__Alter_columns_min.sql
+++
@@ -1,2 +0,0 @@
-ALTER table product	ALTER COLUMN name TYPE varchar(20);
-ALTER TABLE product ALTER COLUMN picture_url TYPE varchar(100);
